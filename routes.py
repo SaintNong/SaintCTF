SaintNong/SaintCTF

commit 40eaae527b2d98d6bab3c70b9727e5d435b33edb

Refactor challenge IDs to be strings
This prevents a desynchronisation of challenge IDs as described in issue
 #10.

routes.py
@@ -20,9 +20,9 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager):
     def challenges():
         # A hack to get solve data to still be "virtually" attached to challenges
         challenge_with_solves = challenge_manager.challenges
-        for i, challenge in enumerate(challenge_with_solves):
+        for id_, challenge in challenge_with_solves.items():
             # Find solves with this challenge id
-            solves = Solve.query.filter_by(challenge_id=i).all()
+            solves = Solve.query.filter_by(challenge_id=id_).all()
 
             # Add the username of each solver to challenge['solvers']
             challenge['solvers'] = []
@@ -34,14 +34,12 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager):
     # Setting up challenge downloads
     @app.route('/downloads/<challenge>/<path:filepath>', methods=['GET'])
     def download(challenge, filepath):
-        challenge_files = [c['files'] for c in challenge_manager.challenges if c['id'] == challenge]
-        if len(challenge_files) < 1: # Challenge ID not found
+        challenge = challenge_manager.challenges.get(challenge)
+
+        if challenge is None: # Challenge ID not found
             abort(404) # https://stackoverflow.com/a/69234618
 
-        assert len(challenge_files) == 1, "Multiple challenges cannot have the same ID!"
-        challenge_files = challenge_files[0]
-
-        if filepath in challenge_files:
+        if filepath in challenge['files']:
             return send_from_directory(CHALLENGES_DIRECTORY, challenge + "/" + filepath)
         else:
             abort(404)
@@ -155,18 +153,18 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager):
         submitted_flag = request.form['flag'].strip()
 
         # Look through the challenges to see if the flag matches any of them
-        for i, challenge in enumerate(challenge_manager.challenges):
+        for id_, challenge in challenge_manager.challenges.items():
             # Flag was correct
             if submitted_flag == challenge['flag']:
 
                 # Check if the user already solved the challenge
-                solve = Solve.query.filter_by(user_id=current_user.id, challenge_id=i).first()
+                solve = Solve.query.filter_by(user_id=current_user.id, challenge_id=id_).first()
                 if solve is None:
                     response_message = f"You've earned {challenge['points']} points."
                     current_user.score += challenge['points']
                     db.session.commit()
 
-                    challenge_manager.solve_challenge(i, current_user)
+                    challenge_manager.solve_challenge(id_, current_user)
 
                     return jsonify({'status': 'correct', 'message': response_message})
 

challenges.py
@@ -45,16 +45,16 @@ def time_ago(time):
 
 class ChallengeManager:
     def __init__(self):
-        self.challenges = []
+        self.challenges = {}
         self.read_challenges()
 
     def read_challenges(self):
-        for challenge_name in os.listdir(CHALLENGES_DIRECTORY):
+        for challenge_id in os.listdir(CHALLENGES_DIRECTORY):
             # Get the directory of the challenge
-            challenge_dir = os.path.join(CHALLENGES_DIRECTORY, challenge_name)
+            challenge_dir = os.path.join(CHALLENGES_DIRECTORY, challenge_id)
 
             # Read the challenge configuration file
-            toml_file = os.path.join(CHALLENGES_DIRECTORY, challenge_name, "challenge.toml")
+            toml_file = os.path.join(CHALLENGES_DIRECTORY, challenge_id, "challenge.toml")
             if os.path.isfile(toml_file):
                 with open(toml_file, "rb") as file:
                     challenge_data = tomllib.load(file)
@@ -62,10 +62,7 @@ class ChallengeManager:
                     # Fix newlines
                     challenge_data['description'] = challenge_data['description'].replace('\n', '<br>')
             else:
-                raise FileNotFoundError(f"Could not find challenge.toml file for challenge '{challenge_name}'")
-
-            # Save the folder name (id) of the challenge
-            challenge_data['id'] = challenge_name
+                raise FileNotFoundError(f"Could not find challenge.toml file for challenge '{challenge_id}'")
 
             # Begin adding challenge files
             challenge_data['files'] = []
@@ -83,18 +80,18 @@ class ChallengeManager:
                 challenge_data['files'].append(entry.name)
 
             # Add the challenge
-            self.challenges.append(challenge_data)
+            self.challenges[challenge_id] = challenge_data
 
         try:
             # Sort challenges by difficulty
-            self.challenges.sort(key=lambda x: DIFFICULTY_MAPPING[x['difficulty']])
+            self.challenges = dict(sorted(self.challenges.items(), key=lambda x: DIFFICULTY_MAPPING[x[1]['difficulty']]))
         except KeyError:
             # Oh no, someone made a new difficulty
             raise KeyError("It seems someone tried to make a new difficulty...")
 
     @staticmethod
-    def solve_challenge(index, user):
-        solve = Solve(user_id=user.id, challenge_id=index)
+    def solve_challenge(id_, user):
+        solve = Solve(user_id=user.id, challenge_id=id_)
         db.session.add(solve)
         db.session.commit()
 

models.py
@@ -24,7 +24,7 @@ class User(db.Model, UserMixin):
 class Solve(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    challenge_id = db.Column(db.Integer, nullable=False)
+    challenge_id = db.Column(db.String, nullable=False)
     time = db.Column(db.DateTime, default=datetime.now)  # Who cares about UTC, this isn't a global CTF
 
     user = db.relationship('User', backref=db.backref('solve', lazy=True))

templates/challenges.html
@@ -119,8 +119,8 @@
 {% include 'navbar.html' %}
 
 <main id="challenge-main">
-    {% set solved_count = challenges | selectattr("solvers", "contains", current_user.username) | list | length %}
-    {% set unsolved_count = challenges | rejectattr("solvers", "contains", current_user.username) | list | length %}
+    {% set solved_count = challenges.values() | selectattr("solvers", "contains", current_user.username) | list | length %}
+    {% set unsolved_count = challenges.values()| rejectattr("solvers", "contains", current_user.username) | list | length %}
 
     <section id="flag-section">
         <h1>Welcome {{ current_user.username }}!</h1>
@@ -168,7 +168,7 @@
     <section id="challenge-list">
         <p>{{ unsolved_count }} challenge(s) left to solve</p>
 
-        {% for challenge in challenges %}
+        {% for id, challenge in challenges.items() %}
             <article class="challenge" data-category="{{ challenge.category }}"
                      data-difficulty="{{ challenge.difficulty }}"
                      {% if current_user.username in challenge.solvers %}
@@ -189,7 +189,7 @@
                     <div class="files">
                         <ul>
                             {% for file in challenge.files %}
-                                <li><a href="downloads/{{ challenge.id }}/{{ file }}" download="{{ file }}">{{ file }}</a></li>
+                                <li><a href="downloads/{{ id }}/{{ file }}" download="{{ file }}">{{ file }}</a></li>
 
                             {% endfor %}
                         </ul>

commit d0c24abf1838663cc6a68818ddbc1fd487cb686c

Hotfix: Fix downloads regression
Fixes an unintentional feature regression was introduced in commit
40eaae5, where the challenge ID was not used when serving a file on the
downloads route.

routes.py
@@ -32,15 +32,15 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager):
         return render_template('challenges.html', challenges=challenge_with_solves, user=current_user)
 
     # Setting up challenge downloads
-    @app.route('/downloads/<challenge>/<path:filepath>', methods=['GET'])
-    def download(challenge, filepath):
-        challenge = challenge_manager.challenges.get(challenge)
+    @app.route('/downloads/<challenge_id>/<path:filepath>', methods=['GET'])
+    def download(challenge_id, filepath):
+        challenge = challenge_manager.challenges.get(challenge_id)
 
         if challenge is None: # Challenge ID not found
             abort(404) # https://stackoverflow.com/a/69234618
 
         if filepath in challenge['files']:
-            return send_from_directory(CHALLENGES_DIRECTORY, challenge + "/" + filepath)
+            return send_from_directory(CHALLENGES_DIRECTORY, challenge_id + "/" + filepath)
         else:
             abort(404)
 