27f33e972de9edd0616393c6f9e703da9b3c84a4
Alexolotal <alexolotal@protonmail.com>
2024-05-13 22:26:21 +0800

    Created first flag of church_of_jowett challenge to teach people how JWT tokens work

--- challenges/grades-grades-grades/container/test-docker.py
+++
@@ -1,7 +0,0 @@
-import docker
-import os
-
-os.path.dirname(__file__)
-client = docker.from_env()
-client.images.build(tag="chall", path=os.path.dirname(__file__))
-client.containers.run("chall", auto_remove=True, detach=True, ports={3001: 3001})
