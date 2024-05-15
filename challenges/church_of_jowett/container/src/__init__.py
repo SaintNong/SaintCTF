25e5a00ec62fa384259e786a02b4483d3be2304e
Alexolotal <Alexolotal@protonmail.com>
2024-05-15 11:43:37 +0800

    Reformat with Black

--- challenges/church_of_jowett/container/src/__init__.py
+++ challenges/church_of_jowett/container/src/__init__.py
@@ -4,7 +4,7 @@ from src import auth
 
 
 def create_app():
-    app = Flask(__name__, static_folder="static",  static_url_path='')
+    app = Flask(__name__, static_folder="static", static_url_path="")
     app.auth = auth
     app.register_blueprint(api)
     return app
