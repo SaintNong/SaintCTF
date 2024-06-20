SaintNong/SaintCTF

commit 3e473ce950685d26d1a2d40186768d75da7d0de3

Remove `User.score`
We now dynamically calculate the user's score based on their solves,
thus preventing the score from desyncing if challenge point values are
adjusted after they have been solved.

routes.py
@@ -31,12 +31,7 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
 
     @event.listens_for(User, "after_update")
     def userTableChanged(_mapper, connection, _target):
-        top_players = connection.execute(
-            select(User.username, User.score, User.id).order_by(User.score.desc())
-        ).all()
-        result = []
-        for line in top_players:
-            result.append({"username": line[0], "score": line[1], "user_id": line[2]})
+        result = challenge_manager.get_top_players()
         sse_queue.put(("leaderboard", json.dumps(result)))
 
     @event.listens_for(Solve, "after_insert")
@@ -63,15 +58,10 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
         #       user table; all other scoring information used in the chart comes
         #       from the solve table.
         # Get top 10 players
-        top_players = (
-            db.session.query(User.id, User.score)
-            .order_by(User.score.desc())
-            .limit(10)
-            .all()
-        )
+        top_players = challenge_manager.get_top_players()[:10]
         top_ids = []
         for player in top_players:
-            top_ids.append(player.id)
+            top_ids.append(player["user_id"])
 
         # Get datapoints
         data_points = challenge_manager.get_leaderboard_chart_data(top_ids)
@@ -108,8 +98,14 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
         # (in case challenges have been removed)
         solves = [x for x in solves if x.challenge_id in challenge_manager.challenges]
 
+        # Save list of user's solved challenges
+        user_solves = [x for x in solves if x.user == current_user]
+
+        # Calculate score
+        score = challenge_manager.get_total_points(user_solves)
+
         # Save count of user's solved challenges
-        solved_count = len([x for x in solves if x.user == current_user])
+        solved_count = len(user_solves)
 
         # Group solves by challenge_id
         # https://stackoverflow.com/a/51416299
@@ -124,6 +120,7 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
             solves=solves,
             solved_count=solved_count,
             user=current_user,
+            score=score,
         )
 
     # Setting up challenge downloads
@@ -276,8 +273,11 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
         if displayed_user is None:
             abort(404, "This player does not exist.")
 
+        solves = Solve.query.filter_by(user_id=user_id).order_by(Solve.time.asc()).all()
+        score = challenge_manager.get_total_points(solves)
+
         # For solve table
-        solves = challenge_manager.get_user_solved_challenges(displayed_user.id)
+        solves = challenge_manager.get_user_solved_challenges(solves)
 
         # For graph
         user_graph_datapoints = challenge_manager.get_user_profile_graph(
@@ -285,20 +285,17 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
         )
 
         # Calculate user rank
-        top_players = (
-            db.session.query(User.username, User.score, User.id)
-            .order_by(User.score.desc())
-            .all()
-        )
+        top_players = challenge_manager.get_top_players()
         rank = 1
         for player in top_players:
-            if player.id == user_id:
+            if player["user_id"] == user_id:
                 break
             rank += 1
 
         return render_template(
             "profile.html",
             user=current_user,
+            score=score,
             displayed_user=displayed_user,
             graph_datapoints=user_graph_datapoints,
             solves=solves,

challenges.py
@@ -244,16 +244,12 @@ class ChallengeManager:
             return tomlkit.load(file)
 
     def solve_challenge(self, id_, user):
-        points = self.challenges[id_]["points"]
-        user.score += points
-
         solve = Solve(user_id=user.id, challenge_id=id_)
         db.session.add(solve)
         db.session.commit()
 
     # Gets the all challenges the user has solved, and how long ago they solved it
-    def get_user_solved_challenges(self, user_id):
-        solves = Solve.query.filter_by(user_id=user_id).order_by(Solve.time.asc()).all()
+    def get_user_solved_challenges(self, solves):
         return [
             {
                 "time": solve.time,
@@ -262,6 +258,22 @@ class ChallengeManager:
             }
             for solve in solves
         ]
+
+    def get_total_points(self, solves):
+        return sum([self.challenges[solve.challenge_id]["points"] for solve in solves])
+
+    def get_top_players(self):
+        top_players = User.query.all()
+        result = []
+        for user in top_players:
+            result.append(
+                {
+                    "username": user.username,
+                    "score": self.get_total_points(user.solve),
+                    "user_id": user.id,
+                }
+            )
+        return sorted(result, key=lambda x: x["score"], reverse=True)
 
     # Gets the datapoints for a profile graph of a specific user id
     def get_user_profile_graph(self, user_id):

models.py
@@ -10,13 +10,12 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    score = db.Column(db.Integer, nullable=True)
 
     def is_active(self):
         return True
 
     def __repr__(self):
-        return f"<User '{self.id}', '{self.username}', '{self.score}>"
+        return f"<User '{self.id}', '{self.username}'>"
 
     def get_id(self):
         return self.id

templates/challenges.html
@@ -19,7 +19,7 @@
     <div id="flag-section-container">
         <section id="flag-section">
             <h1>Welcome {{ current_user.username }}!</h1>
-            <p>You have {{ current_user.score }} points.</p>
+            <p>You have {{ score }} points.</p>
 
             <h2>Filter Challenges</h2>
             <p><span id="visible-count">{{ unsolved_count }}</span> / {{ challenges|length }} challenge(s) visible</p>

templates/profile.html
@@ -21,7 +21,7 @@
     <section id="user-profile-card">
         <h2 id="username-heading">{{ displayed_user.username }}</h2>
         <h2>Rank {{ rank }}</h2>
-        <h2>{{ displayed_user.score }} Points</h2>
+        <h2>{{ score }} Points</h2>
     </section>
 
     <div id="chart-container">

commit a26855b972fc7c76ea3c2e07babf746b5d707209

Hotfix: dispatch SSE for leaderboard table
Fixes a bug introduced in commit 3e473ce; previously, the leaderboard
table's server-sent event depended on updates to `User`, as this was
where the user scores were located.

As `User.score` no longer exists, we now update the leaderboard table
whenever `Solve` receives an insertion event.

routes.py
@@ -29,11 +29,6 @@ first_blood = []
 def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
     app.logger.info("Routes registered")
 
-    @event.listens_for(User, "after_update")
-    def userTableChanged(_mapper, connection, _target):
-        result = challenge_manager.get_top_players()
-        sse_queue.put(("leaderboard", json.dumps(result)))
-
     @event.listens_for(Solve, "after_insert")
     def solveTableChanged(_mapper, connection, _target):
         # Get 'first blood' solves
@@ -51,6 +46,9 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
         recent_solves = challenge_manager.get_recent_solves(12, first_blood)
         sse_queue.put(("recentActivity", json.dumps(recent_solves)))
 
+        top_players = challenge_manager.get_top_players()
+        sse_queue.put(("leaderboard", json.dumps(top_players)))
+
         # Update leaderboard chart
         # NOTE: This relies on *both* the user table and solve table, and ideally,
         #       the event would be dispatched when either one changes - however,
@@ -58,7 +56,7 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
         #       user table; all other scoring information used in the chart comes
         #       from the solve table.
         # Get top 10 players
-        top_players = challenge_manager.get_top_players()[:10]
+        top_players = top_players[:10]
         top_ids = []
         for player in top_players:
             top_ids.append(player["id"])
@@ -346,7 +344,6 @@ def register_routes(app, db, bcrypt, challenge_manager: ChallengeManager, csrf):
             # Send data when event stream is initially connected to
             with app.app_context():
                 with db.engine.connect() as connection:
-                    userTableChanged(None, connection, None)
                     solveTableChanged(None, connection, None)
 
             while True: