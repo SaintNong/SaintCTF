87f66cfdf77f592cb73e9831a4880238333c469e
Alexolotal <Alexolotal@protonmail.com>
2024-06-25 21:10:55 +0800

    Add link to github, and GNU affero license

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+#    SaintCTF: A custom platform for hosting capture the flag tournaments.
+#    Copyright (C) 2024  Ning Xiaozhou, Alex Dennis, Andrew Trang
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import datetime
 import os
 import secrets
