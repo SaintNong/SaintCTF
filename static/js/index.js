73fa7c3050dbb7fa8a6004e6cc1cfca07703399e
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2024-06-26 20:07:01 +0800

    Make help card challenge filter tags clickable

--- static/js/index.js
+++ static/js/index.js
@@ -23,3 +23,17 @@ $('#flag-form-incorrect').submit(function (e) {
         footer: "This alert tells you that you should keep trying &mdash; you are not penalised for this action."
     });
 });
+
+function tagClickHandler(e) {
+    const tag = $(e.currentTarget);
+    const selected = tag.attr('data-selected') !== undefined;
+
+    if (selected) {
+        tag.attr('data-selected', null);
+    } else {
+        tag.attr('data-selected', true);
+    }
+}
+
+$("#difficulty-tag").on('click', tagClickHandler);
+$("#category-tag").on('click', tagClickHandler);
