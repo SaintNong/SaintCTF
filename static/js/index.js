810da165d6b524e90872af36e3c0a965e7b9b5ec
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2024-06-27 12:18:04 +0800

    Remove extraneous comments from index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,6 @@
 $('#flag-form').submit(function (e) {
-    // Prevents default ugly form submission
     e.preventDefault();
 
-    // Extract data
-    let form = $(this);
     Swal.fire({
         title: "Correct flag!",
         html: "You've earned 20 points for <code>My First Challenge!</code>.",
