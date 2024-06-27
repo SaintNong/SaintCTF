96242effff799961a39b3e12a9ddd50b0d51d454
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2024-06-27 18:57:31 +0800

    Use Tippy.js on the solve history page
    Although we denote the meaning of the trophy icon in a legend, this
    change further increases the discoverability for users. Similar to the
    leaderboard, we wrap the emoji in a `<span>` to ensure correct
    positioning of the tooltip.

---
+++ static/js/solves.js
@@ -0,0 +1,9 @@
+tippy("[title]", {
+    content(reference) {
+        const title = reference.getAttribute('title');
+        reference.removeAttribute('title');
+        return title;
+    },
+    delay: [250, null],
+    placement: 'right',
+});
